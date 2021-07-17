936c2d3da0e435c2faa7797839f6a83595826ef8
Wenbin-Xiao <xwb_byron@163.com>
2021-07-17 11:14:51 +0800

    user表增加注册时间，新增管理员表

--- WEB/myweb/sql_user/user.sql
+++ WEB/myweb/sql_user/user.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80018
 File Encoding         : 65001
 
-Date: 2021-07-15 13:36:05
+Date: 2021-07-17 11:14:15
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,16 +23,13 @@ CREATE TABLE `user` (
   `name` varchar(255) NOT NULL,
   `pwd` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `regtime` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('Anne', '123456', '213121@qq.com');
-INSERT INTO `user` VALUES ('Jack', '111111', '123456@qq.com');
-INSERT INTO `user` VALUES ('Tom', '123456', '10086@qq.com');
-INSERT INTO `user` VALUES ('wj', '123456', 'wj@163.com');
-INSERT INTO `user` VALUES ('www', '111111', '111111@qq.com');
-INSERT INTO `user` VALUES ('xf', '111111', '111@163.com');
-INSERT INTO `user` VALUES ('xwb', '123456', 'xwb_byron@163.com');
+INSERT INTO `user` VALUES ('xwb', 'e301e09074272624a246ebe0d25ded764018241a', 'xwb_byron@163.com', '2021-07-09 10:52:00');
+INSERT INTO `user` VALUES ('xwb_test', 'd9ee2c4ee1f60fbd61c524c259fd57c9416dd0c8', 'xwb_byron@163.com', '2021-07-17 10:57:12');
+INSERT INTO `user` VALUES ('xwb1', 'b55c98b55d061ba98cd8e749de603c7593741ba8', 'xwb_byron@163.com', '2021-07-17 10:58:58');
